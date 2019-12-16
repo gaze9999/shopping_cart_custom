645995b7612776e8008f6448cb8f5aea15429efc
gaze9999@iii <gaze9999@livemail.tw>
2019-12-16 15:30:36 +0800

    move card out

---
+++ tpl/itemCard.php
@@ -0,0 +1,14 @@
+<div class="col-md-4 col-sm-6 filter-items" data-price="<?php echo $arr[$i]['itemPrice']; ?>">
+    <div class="card mb-3 shadow-sm itemListCard">
+        <a class="card-img-top list-item itemListImg d-flex center-all" href="./itemDetail.php?itemId=<?php echo $arr[$i]['itemId']; ?>">
+            <img class="img-fluid" src="./images/items/<?php echo $arr[$i]['itemImg']; ?>">
+        </a>
+        <div class="card-body">
+            <p class="card-text list-item-card itemListText"><?php echo $arr[$i]['itemName']; ?></p>
+            <div class="d-flex center-all flex-column">
+                <small class="itemListPrice">價格：<?php echo $arr[$i]['itemPrice']; ?></small>
+                <small class="itemListCTime">上架日期：<?php echo $arr[$i]['created_at']; ?></small>
+            </div>
+        </div>
+    </div>
+</div>
