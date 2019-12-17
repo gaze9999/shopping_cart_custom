9f1cf9b432610246541d65e377955b625191591f
gaze9999@iii <gaze9999@livemail.tw>
2019-12-17 16:30:19 +0800

    add rating // not finish

--- tpl/itemCard.php
+++ tpl/itemCard.php
@@ -1,3 +1,31 @@
+<?php
+    $sqlRatSum = "SELECT m.`rating`, m.`item_id`, i.`itemId`, SUM(rating) AS 'rating' ";
+    $sqlRatSum.= "FROM `comments` as m INNER JOIN `items` as i ";
+    $sqlRatSum.= "ON m.`item_id` = i.`itemId` ";
+    $sqlRatSum.= "GROUP BY i.`itemId` ";
+
+    // $sqlRatSum.= "WHERE m.`item_id` = ? ";
+
+    $stmtRatSum = $pdo->prepare($sqlRatSum);
+    $stmtRatSum->execute();
+
+    // $RatSumPARAM = 1;
+
+    // echo "<pre>";
+    // print_r($arr);
+    // echo "</pre>";
+
+    $stmtRatSum = $pdo->prepare($sqlRatSum);
+    $stmtRatSum->execute();
+    $stmtRatSum = $stmtRatSum->fetchAll(PDO::FETCH_ASSOC);
+
+    // echo "<pre>";
+    // print_r($stmtRatSum);
+    // echo "</pre>";
+    // exit;
+
+?>
+
 <div class="mx-2 filter-items" data-price="<?php echo $arr[$i]['itemPrice']; ?>">
     <div class="card mb-3 shadow-sm itemListCard">
         <a class="card-img-top list-item itemListImg d-flex center-all" href="./itemDetail.php?itemId=<?php echo $arr[$i]['itemId']; ?>">
@@ -6,6 +34,14 @@
         <div class="card-body">
             <p class="card-text list-item-card itemListText"><?php echo $arr[$i]['itemName']; ?></p>
             <div class="d-flex center-all flex-column">
+                <small class="itemListPrice">總讚數：<?php
+                
+                if (isset($stmtRatSum)) {
+                    echo $stmtRatSum[$i]['rating']; 
+                } else {
+                    echo "0";
+                }
+                ?></small>
                 <small class="itemListPrice">價格：<?php echo $arr[$i]['itemPrice']; ?></small>
                 <small class="itemListCTime">上架日期：<?php echo $arr[$i]['created_at']; ?></small>
             </div>
