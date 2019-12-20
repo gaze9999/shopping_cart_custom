7736d967eed3434fe5c81875aade379fa0a74ed9
gaze9999 <gaze9999@livemail.tw>
2019-12-21 04:28:58 +0800

    rename to tpl

--- itemList.php
+++ itemList.php
@@ -14,34 +14,38 @@ require_once("./tpl/func-getRecursiveCategoryIds.php");
 
         <!-- 商品項目清單 -->
         <div class="col-md-8 col-sm-6">
-            <div class="d-flex flex-wrap">
-        <?php
-        if(isset($_GET['categoryId'])){
-            $strCategoryIds = "";
-            $strCategoryIds.= $_GET['categoryId'];
-            getRecursiveCategoryIds($pdo, $_GET['categoryId']);
-        }
+            <div class="row">
+            <?php
+            if(isset($_GET['categoryId'])){
+                $strCategoryIds = "";;
+                $strCategoryIds.= $_GET['categoryId'];
+                getRecursiveCategoryIds($pdo, $_GET['categoryId']);
+            }
 
-        //SQL 敘述
-        $sql = "SELECT i.`itemId`, i.`itemName`, i.`itemImg`, i.`itemPrice`, i.`itemQty`, i.`itemCategoryId`, i.`created_at`, i.`updated_at`, c.`categoryName`, COALESCE(SUM(m.`rating`), 0) as 'ratingSum'
-                FROM `items` as i INNER JOIN `categories` as c LEFT JOIN `comments` as m
-                ON i.`itemCategoryId` = c.`categoryId` AND m.`itemId` = i.`itemId` ";
+            //SQL 敘述
+            $sql = "SELECT i.`itemId`, i.`itemName`, i.`itemImg`, i.`itemPrice`, i.`itemQty`, i.`itemCategoryId`, i.`created_at`, i.`updated_at`, c.`categoryName`, COALESCE(SUM(m.`rating`), 0) as 'ratingSum'
+                    FROM `items` as i INNER JOIN `categories` as c LEFT JOIN `comments` as m
+                    ON i.`itemCategoryId` = c.`categoryId` AND m.`itemId` = i.`itemId` ";
 
-        if(isset($_GET['categoryId'])) {$sql .= "WHERE i.`itemCategoryId` in ({$strCategoryIds}) ";}
+            //若網址有商品種類編號，則整合字串來操作 SQL 語法
+            if(isset($_GET['categoryId'])) {$sql .= "WHERE i.`itemCategoryId` in ({$strCategoryIds}) ";}
             $sql .= "GROUP BY i.`itemId` ";
             $sql .= "ORDER BY i.`itemId` ASC ";
-            
+
             //查詢分頁後的商品資料
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
-    
+
             //若商品項目個數大於 0，則列出商品
             if($stmt->rowCount() > 0) {
                 $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 for($i = 0; $i < count($arr); $i++) {
             ?>
-            <?php require('./tpl/itemCard.php'); ?>
-        <?php } } ?>
+            <?php require('./tpl/tpl-itemCard.php'); ?>                
+            <?php
+                }
+            }
+            ?>
             </div>
         </div>
 
--- tpl/tpl-itemCard.php
+++ tpl/tpl-itemCard.php
@@ -1,14 +1,13 @@
 <div class="mx-2 filter-items" data-price="<?php echo $arr[$i]['itemPrice']; ?>">
     <div class="card mb-3 shadow-sm itemListCard">
-        <a class="card-img-top list-item itemListImg d-flex center-all" href="./itemDetail.php?itemId=<?php echo $arr[$i]['itemId']; ?>">
+        <a class="card-img-top list-item itemListImg" href="./itemDetail.php?itemId=<?php echo $arr[$i]['itemId']; ?>">
             <img class="img-fluid" src="./images/items/<?php echo $arr[$i]['itemImg']; ?>">
         </a>
         <div class="card-body">
             <p class="card-text list-item-card itemListText"><?php echo $arr[$i]['itemName']; ?></p>
             <div class="d-flex center-all flex-column">
                 <small class="itemListPrice">
-                <?php if (isset($stmt)) { echo "總讚數：".$arr[$i]['ratingSum']; } ?>
-
+                    <?php if (isset($stmt)) { echo "總讚數：".$arr[$i]['ratingSum']; } ?>
                 </small>
                 <small class="itemListPrice">價格：<?php echo $arr[$i]['itemPrice']; ?></small>
                 <small class="itemListCTime">上架日期：<?php echo $arr[$i]['created_at']; ?></small>
