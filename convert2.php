882f2979f26e1263dae5cb16eb24754541f73947
gaze9999 <gaze9999@livemail.tw>
2019-12-27 17:04:52 +0800

    update new sql insert

---
+++ convert2.php
@@ -0,0 +1,52 @@
+<?php
+require_once('./vendor/autoload.php');
+require_once('./db.inc.php');
+
+$inputFileName = './test3.xlsx';
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
+
+$highestRow = $spreadsheet->getActiveSheet()->getHighestRow();
+$sheetVar = $spreadsheet->getSheet(0);
+
+$Sql = "INSERT INTO `sake_items` 
+                  (`itemName`, `breId`, `riceId`, `vId`, `alcohol`, 
+                   `nihonshudo`, `aminosando`, `sando`, `seimaibuai`, `capacity`, 
+                   `price` )
+                --    `description`, `howTo`, `nearby`) 
+           VALUES (?, ?, ?, ?, ?, 
+                   ?, ?, ?, ?, ?, 
+                   ? ) ";
+                    // ?, ?, ? ) ";
+
+for($i = 2; $i <= $highestRow; $i++) {
+    if( $sheetVar->getCell('A'.$i)->getValue() === '' || $sheetVar->getCell('A'.$i)->getValue() === null ) break;
+
+    $varParam = [
+        $sheetVar->getCell('E'.$i)->getValue(),
+        $sheetVar->getCell('D'.$i)->getValue(),
+        $sheetVar->getCell('N'.$i)->getValue(),
+        $sheetVar->getCell('F'.$i)->getValue(),
+        $sheetVar->getCell('K'.$i)->getValue(),
+
+        $sheetVar->getCell('J'.$i)->getValue(),
+        $sheetVar->getCell('G'.$i)->getValue(),
+        $sheetVar->getCell('H'.$i)->getValue(),
+        $sheetVar->getCell('I'.$i)->getValue(),
+        $sheetVar->getCell('L'.$i)->getValue(),
+
+        $sheetVar->getCell('M'.$i)->getValue()
+        // $sheetVar->getCell('O'.$i)->getValue(),
+        // $sheetVar->getCell('P'.$i)->getValue(),
+        // $sheetVar->getCell('Q'.$i)->getValue()
+    ];
+
+    $stmtVar = $pdo->prepare($Sql);
+
+    // echo "<pre>";
+    // print_r($stmtVar);
+    // echo $stmtVar;
+    // echo "</pre>";
+    // exit;
+        
+    $stmtVar->execute($varParam);
+}
